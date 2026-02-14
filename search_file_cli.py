54a832b2b6a8aeddcc8eb20c5e8e769b44b4c754
Dorzh <Molotkov Dorzh>
2026-02-14 23:01:01 +0300

    Added string search by attached files and folders

--- search_file_cli.py
+++ search_file_cli.py
@@ -1,31 +1,50 @@
 import sys
+import os
 
-def search_in_file(filepath, search_text):
+def search_in_file(filepath, search_text, verbose=True):
     try:
         with open (filepath, 'r', encoding='utf-8') as f:
             found = 0
             for line_num, line in enumerate(f, start=1):
                 if search_text in line:
-                    print(f"Строка {line_num}: {line.rstrip()}")
+                    if verbose:
+                        print(f"Строка {line_num}: {line.rstrip()}")
                     found += 1
                 return found
-    except FileNotFoundError:
-        print(f"Ошибка: файл {filepath} не найден")
-        return -1
+    except (UnicodeDecodeError, PermissionError):
+        return 0
     except Exception as e:
-        print(f"Error read file {filepath}")
-        return -1
+        return 0
     
+def search_in_dir(dir, search_text, extensions=None):
+    total_found = 0
+    for root, dirs, files in os.walk(dir):
+        for file in files:
+            filepath = os.path.join(root, file)
+            if extensions:
+                ext = os.path.splitext(file)[1].lower()
+                if ext not in extensions:
+                    continue
+
+            found = search_in_file(filepath, search_text, verbose=True)
+            total_found += found
+
+    return total_found
+
 def main():
     if len(sys.argv) != 3:
-        print("Использование: python search_file_cli.py <file> <text>")
+        print("Использование: python search_file_cli.py <dir> <text>")
         sys.exit(1)
 
-    filepath = sys.argv[1]
+    dir = sys.argv[1]
     search_text = sys.argv[2]
 
-    print(f"Поиск '{search_text}' в файле {filepath}")
-    count = search_in_file(filepath, search_text)
+    if not os.path.isdir(dir):
+        print(f"Error - {dir} is not directory")
+        sys.exit(1)
+
+    print(f"Поиск '{search_text}' в директории {dir}")
+    count = search_in_dir(dir, search_text, extensions=['.txt'])
 
     if count >= 0:
         print(f"Всего вхождений: {count}")
